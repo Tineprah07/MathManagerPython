9c722aff128541dc11facf543dea63bfb2415118
Augustine Gyamprah <gyamprahaugustine07@gmail.com>
2025-03-27 10:45:21 +0000

    update

--- mathmanager.py
+++ mathmanager.py
@@ -8,3 +8,23 @@ class mathmanager:
 
 	def multiply(self, a, b):
 			return a*b
+
+	def calculate_monthly_interest(self, principal, years):
+			rate = 0.038 if years == 1 else 0.036
+			monthly_interest = (principal * rate) / 12
+			return round(monthly_interest, 2)
+	
+	def calculate_tax(self, income):
+			tax = 0
+			if income <= 12570:
+				return 0
+			elif income <= 50270:
+				tax += (income - 12570) * 0.20
+			elif income <= 125140:
+				tax += (50270 - 12570) * 0.20
+				tax += (income - 50270) * 0.40
+			else:
+				tax += (50270 - 12570) * 0.20
+				tax += (125140 - 50270) * 0.40
+				tax += (income - 125140) * 0.45
+			return round(tax, 2)
--- mathmanagertest.py
+++ mathmanagertest.py
@@ -2,14 +2,28 @@ import unittest
 from mathmanager import mathmanager
 
 class mathmanagertest(unittest.TestCase):
-	def testAdd(self):
-		math = mathmanager()
-		self.assertEqual(math.add(0, 3), 3)
 
-	def testSubtract(self):
-		math = mathmanager()
-		self.assertEqual(math.subtract(0, 3), -3)
+    def setUp(self):
+        self.mm = mathmanager()
 
-	def testMultiply(self):
-		math = mathmanager()
-		self.assertEqual(math.multiply(0, 3), 0)
+    def testAdd(self):
+        self.assertEqual(self.mm.add(0, 3), 3)
+
+    def testSubtract(self):
+        self.assertEqual(self.mm.subtract(0, 3), -3)
+
+    def testMultiply(self):
+        self.assertEqual(self.mm.multiply(0, 3), 0)
+
+    def test_calculate_monthly_interest(self):
+        self.assertAlmostEqual(self.mm.calculate_monthly_interest(1200, 1), 3.8)
+        self.assertAlmostEqual(self.mm.calculate_monthly_interest(1200, 2), 3.6)
+
+    def test_calculate_tax(self):
+        self.assertEqual(self.mm.calculate_tax(10000), 0)
+        self.assertEqual(self.mm.calculate_tax(20000), 1486.0)
+        self.assertEqual(self.mm.calculate_tax(60000), 11432.0)
+        self.assertEqual(self.mm.calculate_tax(130000), 39675.0) 
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
